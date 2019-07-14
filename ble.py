96c7756fc14fc41221f9d89da66e932fa0f1f117
Sebastien <sebastien-ferry@users.noreply.github.com>
2019-07-14 11:21:57 +0800

    Presqu'init

---
+++ ble.py
@@ -0,0 +1,137 @@
+from flask import Flask
+from flask import render_template
+from flask import request,jsonify
+
+import datetime # Test PsycopPG2
+import time
+import db
+
+app = Flask(__name__)
+
+# debug mode!
+app.debug = True
+from werkzeug.debug import DebuggedApplication
+app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+
+# @app.after_request
+# def set_response_headers(response):
+#     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+#     response.headers['Pragma'] = 'no-cache'
+#     response.headers['Expires'] = '0'
+#     return response
+
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 0
+    response.cache_control.no_cache = True
+    response.cache_control.public = True
+    return response
+
+@app.route('/jinja', defaults={'path': ''})
+@app.route('/jinja/<path:path>')
+def route_jinja(path):
+    db_connection, db_cursor = db.db_connect()
+    t_wordcloud = db.db_select_word_cloud(db_connection, db_cursor)
+    db.db_close(db_connection, db_cursor)
+
+    return render_template('jinja.html', t_wordcloud=t_wordcloud)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def route_index(path):
+    time_now = time.strftime("%H:%M", time.localtime())
+
+    db_connection, db_cursor = db.db_connect()
+    #t_recap = db.db_select_recap(db_connection, db_cursor)
+    ##t_device = db.db_select_device(db_connection, db_cursor)
+    ##t_scan = db.db_select_scan(db_connection, db_cursor)
+    ##t_scan_data = db.db_select_scan_data(db_connection, db_cursor)
+    #t_scan_bucket = db.db_select_scan_bucket(db_connection, db_cursor)
+    db.db_close(db_connection, db_cursor)
+
+    return render_template('index.html', date=time_now) #,t_recap=[], t_device=t_device, t_scan=t_scan, t_scan_data=t_scan_data, t_scan_bucket=t_scan_bucket)
+
+@app.route('/device')
+@app.route('/device/<p_limit>')
+@app.route('/device/<p_address>/<p_address_type>')
+def route_device(p_address=None, p_address_type=None, p_limit=100):
+    time_now = time.strftime("%H:%M", time.localtime())
+
+    db_connection, db_cursor = db.db_connect()
+    t_device = db.db_select_device(db_connection, db_cursor, p_address, p_address_type, p_limit)
+    db.db_close(db_connection, db_cursor)
+    return render_template('device.html', date=time_now, t_device=t_device)
+
+@app.route('/scan_data')
+@app.route('/scan_data/<p_limit>')
+@app.route('/scan_data/<p_address>/<p_address_type>')
+def route_scan_data(p_address=None, p_address_type=None, p_limit=10):
+    time_now = time.strftime("%H:%M", time.localtime())
+
+    db_connection, db_cursor = db.db_connect()
+    t_scan_data = db.db_select_scan_data(db_connection, db_cursor, p_address, p_address_type, p_limit)
+    db.db_close(db_connection, db_cursor)
+    return render_template('scan_data.html', date=time_now, t_scan_data=t_scan_data)
+
+@app.route('/car')
+@app.route('/scan_data_car')
+def route_scan_data_car():
+    time_now = time.strftime("%H:%M", time.localtime())
+
+    db_connection, db_cursor = db.db_connect()
+    t_scan_data_car = db.db_select_scan_data_car(db_connection, db_cursor)
+    db.db_close(db_connection, db_cursor)
+    return render_template('scan_data_car.html', date=time_now, t_scan_data_car=t_scan_data_car)
+
+@app.route('/test/<path:subpath>')
+def route_test(subpath):
+    time_now = time.strftime("%H:%M", time.localtime())
+
+    return render_template("t_" + subpath + '.html', date=time_now)
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+@app.route('/ajax', methods=['GET','POST'])
+def route_ajax():
+    time_now = time.strftime("%H:%M:%S", time.localtime())
+
+    if request.method == "GET":
+        params = request.args.getlist('name')
+    elif request.method == "POST":
+        params = request.form.getlist('name')
+    else:
+        params = []
+
+    return jsonify(time=time_now, args=params)
+
+@app.route('/ajax_db', methods=['GET','POST'])
+def route_ajax_db():
+    time_now = time.strftime("%H:%M:%S", time.localtime())
+
+    if request.method == "GET":
+        restrict = request.args.getlist('restrict')
+        query = request.args.get('query')
+    elif request.method == "POST":
+        restrict = request.form.getlist('restrict')
+        query = request.form.get('query')
+    else:
+        restrict = []
+        query = ""
+
+    t_result = []
+    db_connection, db_cursor = db.db_connect()
+    if query == "db_select_scan_bucket":
+        t_result = db.db_select_scan_bucket(db_connection, db_cursor)
+    elif query == "db_select_recap":
+        t_result = db.db_select_recap(db_connection, db_cursor, restrict)
+
+    db.db_close(db_connection, db_cursor)
+
+    return jsonify(time=time_now, result=t_result)
+
+if __name__ == "__main__":
+        app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,215 @@
+import psycopg2
+
+def db_connect():
+    try:
+        db_connection = psycopg2.connect( user = "ble", password = "ble"
+                                     , host = "t4.lan", port = "5432"
+                                     , database = "ble")
+        db_cursor = db_connection.cursor()
+
+    except (Exception, psycopg2.Error) as error :
+        print ("ERROR PsycoPG2 (db_connect) : ", error)
+
+    return db_connection, db_cursor
+
+def db_close(db_connection, db_cursor):
+    try:
+        if (db_connection):
+            db_cursor.close()
+            db_connection.close()
+
+    except (Exception, psycopg2.Error) as error :
+        print ("ERRO PsycoPG2 (db_close): ", error)
+
+    return
+
+def db_select_device(db_connection, db_cursor, address=None, address_type=None, limit=10):
+    sql = """ select d.*, o.identifier
+              from device as d inner join oui as o
+                  on  o.prefix = left(d.address, 8)
+                  and d.address_type = 'public'
+              order by d.lastseen desc"""
+    sql += " limit " + str(limit) + " ;"
+
+    db_cursor.execute(sql)
+    device = db_cursor.fetchall()
+    return device
+
+def db_select_scan(db_connection, db_cursor):
+    sql = """ SELECT * from scan
+             order by timestamp desc
+             limit 10 ;"""
+    db_cursor.execute(sql)
+    scan = db_cursor.fetchall()
+    return scan
+
+def db_select_scan_data(db_connection, db_cursor, address=None, address_type=None, limit=None):
+    sql = """select *
+             from  scan_data
+             where 1=1""" #name in ('Short Local Name', 'Complete Local Name')"""
+    if address is not None and address_type is not None:
+        sql += " and address='" + p_address +"'" \
+             + " and address_type ='" + p_address_type +"'"
+    sql += " order by timestamp desc, address, address_type, name"
+    if limit is not None:
+        sql += " limit " + str(limit) + " ;"
+    else:
+        sql += " limit 30 ;"
+
+    db_cursor.execute(sql)
+    scan_data = db_cursor.fetchall()
+    return scan_data
+
+def db_select_scan_bucket(db_connection, db_cursor):
+    sql = """select day_hour, coalesce(count(scan.*),0)
+    from (	select generate_series(date_trunc('hour', now() - interval '1 day'),now(), '10 min') as day_hour
+    	 ) as timeserie
+    left outer join scan
+    	 on timeserie.day_hour = to_timestamp( (floor(extract(epoch from scan.timestamp at time zone 'UTC-8') / (10*60))*(10*60) ))
+    	 --and scan.timestamp > now() - interval '24:00:00'
+    group by day_hour"""
+
+    db_cursor.execute(sql)
+    scan_data_bucket = db_cursor.fetchall()
+    return scan_data_bucket
+
+def db_select_scan_data_car(db_connection, db_cursor):
+    sql = """ SELECT *
+              FROM scan_data as s
+              WHERE s.value like '沪%'
+              order by timestamp desc; """
+    db_cursor.execute(sql)
+    scan_data_car = db_cursor.fetchall()
+    return scan_data_car
+
+def db_select_recap(db_connection, db_cursor, restrict=None):
+    t_queries = {
+          'device_total' : """
+            select count(1)
+            from device"""
+        , 'device_delta' : """
+            select count(1)
+            from device
+            where lastseen > now() - interval '1 day'
+            """
+        , 'device_bucket' : """
+            select bucket, coalesce(count(s.*),0)
+            FROM (select generate_series(date_trunc('day', now() - interval '7 day'),now(), '1 day')::DATE as bucket) as timeserie
+             left  outer join device as s
+            	on    timeserie.bucket = date_trunc('day', s.lastseen )
+            	and   s.lastseen > now() - interval '7 days'
+            group by bucket
+            order by bucket
+            """
+        , 'name_total' : """
+            select count(1)
+            from scan_data
+            where name in ('Complete local Name', 'Short Local Name')
+            """
+        , 'name_delta' : """
+            select count(1)
+            from scan_data
+            where timestamp > now() - interval '1 day'
+            and   name in ('Complete local Name', 'Short Local Name')
+            """
+        , 'name_bucket' : """
+            select bucket, coalesce(count(s.*),0)
+            FROM (select generate_series(date_trunc('day', now() - interval '7 day'),now(), '1 day')::DATE as bucket) as timeserie
+             left  outer join scan_data as s
+            	on    timeserie.bucket = date_trunc('day', s.timestamp )
+            	and   s.timestamp > now() - interval '7 days'
+                and   name in ('Complete local Name', 'Short Local Name')
+            group by bucket
+            order by bucket
+            """
+        , 'scan_total' : """
+            select count(1)
+            from scan
+            """
+        , 'scan_delta' : """
+            select count(1)
+            from scan
+            where timestamp > now() - interval '1 day'
+            """
+        , 'scan_bucket' : """
+            select bucket, coalesce(count(s.*),0)
+            FROM (select generate_series(date_trunc('day', now() - interval '7 day'),now(), '1 day')::DATE as bucket) as timeserie
+             left  outer join scan as s
+            	on    timeserie.bucket = date_trunc('day', s.timestamp )
+            	and   s.timestamp > now() - interval '7 days'
+            group by bucket
+            order by bucket
+            """
+        , 'car_total' : """
+            SELECT count(1)
+            FROM scan_data as s
+            WHERE 1=1
+            and   s.address_type = 'public'
+            AND   s.name = 'Complete Local Name'
+            AND   s.value like '沪%'
+            """
+        , 'car_delta' : """
+            SELECT count(1)
+            FROM scan_data as s
+            WHERE 1=1
+            and   timestamp > now() - interval '1 day'
+            and   s.address_type = 'public'
+            AND   s.name = 'Complete Local Name'
+            AND   s.value like '沪%'
+            """
+        , 'car_bucket' : """
+            select bucket, coalesce(count(s.*),0)
+            FROM (select generate_series(date_trunc('day', now() - interval '7 day'),now(), '1 day')::DATE as bucket) as timeserie
+             left  outer join scan_data as s
+            	on    timeserie.bucket = date_trunc('day', s.timestamp )
+            	and   s.timestamp > now() - interval '7 days'
+            	and   s.address_type = 'public'
+            	AND   s.name = 'Complete Local Name'
+            	AND   s.value like '沪%'
+            group by bucket
+            order by bucket
+            """
+            }
+    t_result = {}
+    for name, query in t_queries.items():
+        if restrict is None or name in restrict:
+            db_cursor.execute(query)
+            result = db_cursor.fetchall()
+            if db_cursor.rowcount == 1:
+                t_result[name] = "{:,}".format(result[0][0])
+            else:
+                t_result[name] = result
+
+    return t_result
+
+def db_select_word_cloud(db_connection, db_cursor):
+    t_queries = {
+          'name' : """
+            select value,  count(1)
+            from scan_data
+            where name in ('Short Local Name', 'Complete Local Name')
+            group by value
+            having count(1) > 1
+            order by count(1) desc
+            limit 100
+            """
+        , 'manufacturer' : """
+            select identifier, count(1)
+            from device
+            inner join oui
+            on    prefix = left(address, 8)
+            and   address_type = 'public'
+            group by identifier
+            having count(1) > 1
+            order by count(1) desc, identifier
+            limit 100
+            """
+        }
+
+    t_result = {}
+    for name, query in t_queries.items():
+        db_cursor.execute(query)
+        result = db_cursor.fetchall()
+        t_result[name] = result
+
+    return t_result
